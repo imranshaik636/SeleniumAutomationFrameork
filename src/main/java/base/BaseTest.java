80c2f22f77834ff85d4da467e2d1acf5a6930a3d
ImranSahik <imranbashashaik636@gmail.com>
2025-06-01 13:11:22 +0530

    changes added

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,18 +1,22 @@
 package base;
+
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
+
 import utils.EmailUtils;
 import utils.ExtentReprts;
 import utils.Log;
-import org.testng.annotations;
+
 public class BaseTest {
 
 	protected WebDriver driver;
@@ -27,7 +31,7 @@ public class BaseTest {
 	@AfterSuite
 	public void teardownReport() {
 		extent.flush();
-
+		
 		String reportPath = ExtentReprts.reportPath;
 		EmailUtils.sendTestReport(reportPath);
 
